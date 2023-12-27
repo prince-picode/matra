9e17885ed4275ccaa050e64a455d814a39d7c817
kaushik-dhameliya <dhameliyak1@gmail.com>
2023-12-27 14:59:53 +0530

    +init

---
+++ src/assets/js/pages/index.init.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".productSlider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'primary': '#417F56',
+      'primary-80':'#679978',
+      'primary-40':'#B3CCBB',
+      'white': '#ffffff',
+      'black': '#1D252D',
+      'danger': '#FD5154',
+    },
+    fontFamily:{
+      body: ['League Spartan']
+    },
+    extend: {},
+    container:{
+      padding:{
+        DEFAULT:'1rem',
+        sm:'1rem',
+        lg:'45px',
+        xl:'5rem',
+        '2xl':'13rem',
+      },
+      margin:'0 auto',
+    }
+  },
+  plugins: [],
+}
+
