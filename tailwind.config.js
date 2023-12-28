fbb9763b317bc860865718f180809cd041ecbfde
prince-picode <prince@picode.in>
2023-12-28 18:29:28 +0530

    + navbar-hero-s1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,27 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    colors:{
-      'primary': '#417F56',
-      'primary-80':'#679978',
-      'primary-40':'#B3CCBB',
-      'white': '#ffffff',
-      'black': '#1D252D',
-      'danger': '#FD5154',
+    colors: {
+      primary: "#417F56",
+      "primary-80": "#679978",
+      "primary-40": "#B3CCBB",
+      white: "#ffffff",
+      black: "#1D252D",
+      danger: "#FD5154",
     },
-    fontFamily:{
-      body: ['League Spartan']
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "45px",
+        xl: "5rem",
+        "2xl": "13rem",
+      },
+      margin: "0 auto",
     },
-    extend: {},
-   
+    fontFamily: {
+      body: ["League Spartan"],
+    },
   },
   plugins: [],
-}
-
+};
